d3efa1a66bf4eaa22b9045facefdfb5af90498c3
Mathis-zls <mzizlsperger@hotmail.com>
2025-02-11 13:55:36 +0100

    Database Postgres

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Mathis-zls/RSS/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	portSring := os.Getenv("PORT")
 	if portSring == "" {
 		log.Fatal("Port missing")
+	}
+
+	dbString := os.Getenv("DB_URL")
+	if dbString == "" {
+		log.Fatal("DB missing")
+	}
+
+	conn, err := sql.Open("postgres", dbString)
+	if err != nil {
+		log.Fatal("Error Connecting to Database")
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -33,7 +54,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healtz", handlerReady)
 	v1Router.Get("/err", handlerErr)
-
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
@@ -41,7 +62,7 @@ func main() {
 		Addr:    ":" + portSring,
 	}
 	log.Printf("Server start on port %v", portSring)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal()
 	}
---
+++ models.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/Mathis-zls/RSS/internal/database"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created"`
+	UpdatedAt time.Time `json:"lastupdate`
+	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+	}
+}
